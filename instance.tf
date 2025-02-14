5d173721d2f1423acfa84c8946a11b3bc4df5787
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2025-02-14 22:18:05 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,3 +1,10 @@
+resource "google_storage_bucket" "source" {
+  name          = "my-bucket-curl-cloud7"
+  location      = "us-east1"
+  force_destroy = true
+}
+
+
 resource "google_compute_instance" "test-instance" {
   name         = "test-instance"
   machine_type = "e2-micro"
@@ -9,9 +16,19 @@ resource "google_compute_instance" "test-instance" {
     }
   }
 
+  metadata_startup_script = <<EOT
+  set -xe \
+    && sudo apt update gsutil curl -y \
+    && curl -I https://www.google.com > ping.txt \
+    && gsutil cp ping.txt gs://my-bucket-curl-cloud7/ \
+
+EOT
+
   network_interface {
     network    = google_compute_network.vpc.id
     subnetwork = google_compute_subnetwork.subnet-private.id
     access_config {}
   }
+
+  depends_on = [google_storage_bucket.source]
 }
