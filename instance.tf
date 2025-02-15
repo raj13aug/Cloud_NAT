011c0774d4670f49ce3f0f73df820584c03b0283
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2025-02-15 08:40:07 +0530

    v3

--- instance.tf
+++ instance.tf
@@ -3,11 +3,23 @@ data "google_compute_default_service_account" "default" {
   project = var.project_id
 }
 
+resource "google_project_iam_member" "osconfig_agent_role" {
+  project = var.project_id
+  role    = "roles/osconfig.guestPolicyAdmin"
+  member  = "serviceAccount:${data.google_compute_default_service_account.default.email}"
+}
+
+resource "google_project_iam_member" "compute_instance_admin_role" {
+  project = var.project_id
+  role    = "roles/compute.instanceAdmin.v1"
+  member  = "serviceAccount:${data.google_compute_default_service_account.default.email}"
+}
 
 resource "google_compute_instance" "test-instance" {
-  name         = "test-instance"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
+  name                      = "test-instance"
+  machine_type              = "e2-micro"
+  zone                      = "us-central1-a"
+  allow_stopping_for_update = true
 
   metadata = {
     enable-guest-attributes = "TRUE"
@@ -29,6 +41,9 @@ resource "google_compute_instance" "test-instance" {
   metadata_startup_script = <<-EOF
     #!/bin/bash
     sudo apt-get update
+    sudo apt-get install -y google-osconfig-agent
+    sudo systemctl enable google-osconfig-agent
+    sudo systemctl start google-osconfig-agent    
   EOF
 
   network_interface {
